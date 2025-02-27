3091f18420e74f39e0c49c48ce7f7186341aa5b0
Juanjo Matamoros <juanjo3026@gmail.com>
2025-02-27 20:41:42 +0100

    Primera versión del proyecto

---
+++ src/cuentas/cliente/Main.java
@@ -0,0 +1,31 @@
+package cuentas.cliente;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(null);
+    }
+
+    public static void operativa_cuenta(Float cantidad) {
+        Cuenta Cuenta1;
+        double saldoActual;
+
+        Cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        
+        System.out.println("El saldo actual es:"+ Cuenta1.estado() );
+
+        try {
+            Cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ Cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ Cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
